ad885c78a6bd293b7ef38a416c2deff6fb070e09
estevamnetof <estevam.neto07@gmail.com>
2024-05-01 01:52:05 -0300

    argumentos

---
+++ curso-yt/9-argumentos/9-argumentos.md
@@ -0,0 +1,6 @@
+# Argumentos
+
+- Os argumentos são valores dinâmicos que podem ser inseridos em:
+- Diretivas: baseados nestes valores para executar um determinado comportamento
+- Atributos: mudar URL de links ou src de imagens
+- Essenciais para alterar a experiência do usuário no uso da aplicação
---
+++ curso-yt/9-argumentos/aula/src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <PrimeiroComponente />
+    <LifeCycle />
+  </div>
+</template>
+
+<script>
+import PrimeiroComponente from './components/PrimeiroComponente.vue'
+import LifeCycle from './components/LifeCycle.vue'
+import Info from './components/Info.vue'
+
+export default {
+  name: 'App',
+  components: {
+    PrimeiroComponente,
+    LifeCycle,
+    Info
+  }
+}
+</script>
