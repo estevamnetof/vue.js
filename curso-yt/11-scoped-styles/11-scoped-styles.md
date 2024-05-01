228112d950220250d74a0edad1a292e70796e3a5
estevamnetof <estevam.neto07@gmail.com>
2024-05-01 02:21:41 -0300

    global e scoped style

---
+++ curso-yt/11-scoped-styles/11-scoped-styles.md
@@ -0,0 +1,6 @@
+# CSS global e scoped
+
+- O CSS no Vue pode ser dividido em duas categorias
+- Global: Onde definimos no App, por exemplo, e se aplica a todos os elementos
+- Scoped: Onde cada componente pode ter seu estilo, deixando mais fácil de personalizar os elementos
+- Geralmente utilizamos o scoped para estilizar os componentes individualmente
---
+++ curso-yt/11-scoped-styles/aula/src/components/Info.vue
@@ -0,0 +1,54 @@
+<template>
+  <div>
+      <p v-if="esta_trabalhando">Estou trabalhando no momento.</p>
+      <p v-else>Estou em busca de novas oportunidades!</p>
+      <p>utilizo as seguintes tecnologias:</p>
+      <ul>
+          <li>JavaScript</li>
+          <li>PHP</li>
+          <li>Python</li>
+      </ul>
+      <div>
+          <button @click="showEmail">{{ textoBotao }}</button>
+      </div>
+      <p v-show="mostrar_email">Mande uma mensagem para: {{ email }}</p>
+      <p>Para acessar meu portfólio <a v-bind:href="meu_link" target="_blank">Basta clicar aqui</a></p>
+      <Picture />
+  </div>
+</template>
+
+<script>
+  import Picture from './Picture.vue'
+
+  export default {
+      name: 'Info',
+      components: {
+          Picture
+      },
+      data() {
+          return {
+              esta_trabalhando: true,
+              mostrar_email: false,
+              email: 'estevam@email.com',
+              meu_link: 'https://google.com',
+              textoBotao: 'Mostrar e-mail'
+          }
+      },
+      methods: {
+          showEmail() {
+              this.mostrar_email = !this.mostrar_email
+              if(!this.mostrar_email) {
+                  this.textoBotao = 'Mostrar e-mail'
+              } else {
+                  this.textoBotao = 'Esconder e-mail'
+              }
+          }
+      }
+  }
+</script>
+
+<style>
+  .paragrafo-pai {
+      background-color: red;
+  }
+</style>
