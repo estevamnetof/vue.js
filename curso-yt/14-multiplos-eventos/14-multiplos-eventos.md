31601b8c17120e38d0f95a004bac64d6b074d47f
estevamnetof <estevam.neto07@gmail.com>
2024-05-03 07:56:51 -0300

    multiplos eventos

---
+++ curso-yt/14-multiplos-eventos/14-multiplos-eventos.md
@@ -0,0 +1,6 @@
+# Múltiplos eventos
+
+- O Vue também permite a entrada de múltiplos eventos em um único evento
+- A sintaxe permanece a mesma
+- Porém vamos separar os eventos por vírgula
+- Isso permite executar dois ou mais métodos com um click, por exemplo
---
+++ curso-yt/14-multiplos-eventos/aula/src/components/PrimeiroComponente.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+      <h1>Olá Vue!</h1>
+      <p>Meu nome é {{ nome }} e trabalho como {{ profissao }}</p>
+      <MultiplosEventos />
+  </div>
+</template>
+
+<script>
+  import MultiplosEventos from './MultiplosEventos.vue'
+
+  export default {
+      name: 'PrimeiroComponente',
+      components: {
+          MultiplosEventos
+      },
+      data() {
+          return {
+              nome: 'Estevam',
+              profissao: 'Programador'
+          }
+      }
+  }
+</script>
